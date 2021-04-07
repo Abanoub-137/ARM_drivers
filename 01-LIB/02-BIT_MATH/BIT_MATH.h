4c4d1f0e4a38350055e600aa4de554b6040b401c
Abanoub Kamal <abanoubkamal137@gmail.com>
2021-04-07 16:55:28 +0200

    add COTS  files
    add the content of ARM COTS (abanoub)

---
+++ 01-LIB/02-BIT_MATH/BIT_MATH.h
@@ -0,0 +1,19 @@
+/************************************************************************/
+/*	author	:	Abanoub Kamal                                           */
+/* 	version	:	V01														*/
+/*	date	: 	10 NOV 2020												*/
+/************************************************************************/
+#ifndef BIT_MATH_H
+#define BIT_MATH_H
+
+
+#define SET_BIT( data , bit )			( data |= (1 << bit) )
+#define GET_BIT( data , bit )			(( data >> bit ) & 1)
+#define CLR_BIT( data , bit )			( data &= (~(1 << bit)) )
+#define TOG_BIT( data , bit )	   		( data ^= (1 << bit) )
+
+
+
+#endif /* BIT_MATH_H_ */
+
+
